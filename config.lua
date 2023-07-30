da8bffc717380950523661b94154d4b8a4bcf552
B3NDO <140925178+xB3NDO@users.noreply.github.com>
2023-07-30 04:04:29 +0200

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,39 @@
+Config = {}
+
+Config.ListeJobs = {
+    police = {
+        label = "Police",
+        vehicles = {
+            "police",    -- Nom du premier véhicule de police
+            "police2",   -- Nom du deuxième véhicule de police
+            -- Ajoutez d'autres véhicules de police ici
+        }
+    },
+    ambulance = {
+        label = "Ambulance",
+        vehicles = {
+            "ambulance",    -- Nom du premier véhicule d'ambulance
+            "ambulance2",   -- Nom du deuxième véhicule d'ambulance
+            -- Ajoutez d'autres véhicules d'ambulance ici
+        }
+    },
+    mechanic = {
+        label = "Mécanicien",
+        vehicles = {
+            "flatbed",    -- Nom du premier véhicule de mécanicien
+            -- Ajoutez d'autres véhicules de mécanicien ici
+        }
+    },
+    taxi = {
+        label = "Taxi",
+        vehicles = {
+            "taxi",    -- Nom du premier véhicule de taxi
+            -- Ajoutez d'autres véhicules de taxi ici
+        }
+    },
+    -- Ajoutez d'autres et leurs véhicules associés ici
+}
+
+Config.OKOKNotify = true -- Activer OKOKNotify
+
+Config.CheckTime = 1000 -- Intervalle de vérification -NE PAS TOUCHER POUR UNE BONNE OPTIMISATION-
---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'bodacious'
+game 'gta5'
+
+name 'CARJOB'
+author 'B3NDO'
+description 'Un script pour remplacer les clés des voitures d\'entreprise, autorise les jobs à utiliser leurs voitures associées.'
+version 'V.0.1'
+url 'https://github.com/xB3NDO'
+
+server_script {
+    'version.lua'
+}
+
+client_scripts {
+    'config.lua',
+    'client/client.lua'
+}

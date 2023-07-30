4885df3ddd7bbdcc5ffd2bb17a3efc4bcdd122c8
B3NDO <140925178+xB3NDO@users.noreply.github.com>
2023-07-30 17:38:49 +0200

    Delete CARJOB directory

--- CARJOB/version.lua
+++
@@ -1,7 +0,0 @@
-local resourceName = "CARJOB"
-local githubLink = "https://github.com/xB3NDO/CARJOB"
-
-print("  //\n  ||    " .. resourceName .. " est créé par B3NDO\n  ||")
-
-print("  ||    Version actuelle : ^2V.0.2^0")
-print("  ||    Téléchargez ici la dernière version : ^2" .. githubLink .. "^0\n  \\\\")

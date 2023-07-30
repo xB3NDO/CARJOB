2d2789474b7eecfa6e0c6f546870d58c1f511bb7
B3NDO <140925178+xB3NDO@users.noreply.github.com>
2023-07-30 04:34:21 +0200

    Update version.lua

--- version.lua
+++ version.lua
@@ -1,26 +1,7 @@
-local resourceName = "CARJOB" 
+local resourceName = "CARJOB"
+local githubLink = "https://github.com/xB3NDO/CARJOB"
 
-PerformHttpRequest("https://api.github.com/repos/votre_nom_utilisateur/votre_repo/releases/latest", function(err, text, headers)
-    Citizen.Wait(5000)
-    if text then
-        local info = json.decode(text)
-        local currentVersion = GetResourceMetadata(resourceName, "version")
-        local newestVersion = info.tag_name
+print("  //\n  ||    " .. resourceName .. " est créé par B3NDO\n  ||")
 
-        print("  //\n  ||    " .. resourceName .. " est créé par " .. info.author.login .. "\n  ||")
-
-        if newestVersion ~= nil then
-            if newestVersion ~= currentVersion then
-                print("  ||    Version actuelle : ^1" .. currentVersion .. "^0")
-                print("  ||    Dernière version : ^2" .. newestVersion .. "^0\n  ||")
-                print("  ||    ^1Une mise à jour est disponible pour " .. resourceName .. ", visitez " .. info.html_url .. " pour obtenir la dernière version.\n^0  \\\\")
-            else
-                print("  ||    Version actuelle : ^2" .. currentVersion .. "^0")
-                print("  ||    Dernière version : ^2" .. newestVersion .. "^0\n  ||")
-                print("  ||    ^2" .. resourceName .. " est à jour !\n^0  \\\\")
-            end
-        else
-            print("  ||    ^1Une erreur s'est produite lors de la récupération des informations de la dernière version.\n^0  \\\\")
-        end
-    end
-end)
+print("  ||    Version actuelle : ^2V.0.1^0")
+print("  ||    Téléchargez ici la dernière version : ^2" .. githubLink .. "^0\n  \\\\")
